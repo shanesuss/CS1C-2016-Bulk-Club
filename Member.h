98f1f453b9cd0f94fdd8e5cf28aaf78335a29832
shanesuss <shanesuss@gmail.com>
2016-03-07 15:29:39 -0800

    moving method definitions

--- Member.h
+++ Member.h
@@ -57,29 +57,9 @@ public:
 	/* AddPurchase - adds newPurchase to totalSpent (i.e. a new transaction) */
 	void AddPurchase(double newPurchase);
 private:
-	string name;
+	qstring name;
 	int    idNum;
-	string memberType;
+	qstring memberType;
 	Date   expDate;
 	double totalSpent;
 };
-
-Member::~Member() {}
-
-Member::Member()
-{
-	name = "";
-	idNum = 0;
-	memberType = "";
-	expDate.SetDate(0,0,0);
-	totalSpent = 0.0;
-}
-
-Member::Member(string newName, int newID, string newType, Date newDate, double newTotal)
-{
-	name = newName;
-	idNum = newID;
-	memberType = newType;
-	expDate = newDate;
-	totalSpent = newTotal;
-}
