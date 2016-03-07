540206ce64cf9c02f6558f3aa9fa73d48cec7ae1
shanesuss <shanesuss@gmail.com>
2016-03-07 15:38:00 -0800

    Update Date.h

--- Date.h
+++ Date.h
@@ -9,21 +9,39 @@ using namespace std;
 class Date
 {
 public:
+  //Default Constructor
   Date();
+  
+  //Non-default Constructor 
   Date(int month,
        int day,
        int year);
+  
+  //Default Destructor
   ~Date();
+  
+  //MUTATORS
   void SetDate(int month,
                int day,
                int year);
+               
+  //ACCESSORS
   void GetDate(int &month,
                int &day,
                int &year) const;
+               
   int GetYear() const;
+  
   int GetMonth() const;
+  
   int GetDay() const;
+  
   string DisplayDate() const;
+
+  //OVERLOADED OPERATORS
+  bool operator<(Date compareto) const;
+  
+  bool operator>(Date compareto) const;
 
 private:
   int dateMonth;
