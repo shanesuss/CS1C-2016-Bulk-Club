061f29d64133a25f47af24cb0faac67d47a6979f
Kayvon Haghighi <haghighihk@gmail.com>
2016-03-09 13:53:33 -0800

    implementing methods

--- Member.cpp
+++ Member.cpp
@@ -19,3 +19,66 @@ Member::Member(string newName, int newID, string newType, Date newDate, double n
     expDate = newDate;
     totalSpent = newTotal;
 }
+
+void Member::AddPurchase(double newPurchase)
+{
+    totalSpent += newPurchase;
+}
+
+Date Member::GetExpirationDate() const
+{
+    return expDate;
+}
+
+
+int Member::GetId() const
+{
+    return idNum;
+}
+
+string Member::GetMemberInfo() const
+{
+    // to be written later
+    return "";
+}
+
+string Member::GetMemberType() const
+{
+    return memberType;
+}
+
+string Member::GetName() const
+{
+    return name;
+}
+
+double Member::GetTotalSpent() const
+{
+    return totalSpent;
+}
+
+void Member::SetExpirationDate(Date newExpDate)
+{
+    expDate = newExpDate;
+}
+
+void Member::SetID(int newId)
+{
+    idNum = newId;
+}
+
+void Member::SetMemberType(string newMemberType)
+{
+    memberType = newMemberType;
+}
+
+void Member::SetName(int newName)
+{
+    name = newName;
+}
+
+void Member::SetTotalSpent(double newTotalSpent)
+{
+    totalSpent = newTotalSpent;
+}
+
