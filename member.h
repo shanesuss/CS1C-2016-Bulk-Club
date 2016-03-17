f2cb498055d3efca293277fde9a9642697777e87
KermiteO <kermicheo@gmail.com>
2016-03-17 15:17:47 -0700

    made little changes so it compiles right

---
+++ member.h
@@ -0,0 +1,86 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+/***********************************************
+ * Name: Kayvon Haghighi
+ * Class: CS 1C
+ * Project: 1
+************************************************/
+
+#include <QString>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include "Date.h"
+using namespace std;
+
+class Member
+{
+public:
+
+/* Constructors/Destructors */
+    /* Default Constructor - Initialize with default values of 0 */
+    Member();
+
+    /* Non-Default Constructor - Initialize with referenced values */
+    Member(QString newName, int newID, Date newDate, float newTotal);
+
+    /* Destructor - delete instance of object */
+    ~Member();
+
+
+/* Accessors */
+    /* GetName - return name */
+    QString GetName() const;
+
+    /* GetId - return idNum */
+    int GetId() const;
+
+    /* GetMemberType - return memberType */
+    QString GetMemberType() const;
+
+    /* GetExpirationDate - return instance of expDate */
+    Date GetExpirationDate() const;
+
+    /* GetTotalSpent - return totalSpent */
+    float GetTotalSpent() const;
+
+    /* GetMemberInfo - return formatted list of user info */
+    QString GetMemberInfo() const;
+
+/* Modifiers */
+    void SetMember(QString newName,
+                   double  newNum,
+                   Date    newDate,
+                   float   newSpent);
+
+    /* SetName - change name */
+    void SetName(int newName);
+
+    /* SetID - change idNum */
+    void SetID(int newId);
+
+    /* SetMemberType - change memberType */
+    void SetMemberType(QString newMemberType);
+
+    /* SetExpirationDate - change expDate */
+    void SetExpirationDate(Date newExpDate);
+
+    /* SetTotalSpent - change totalSpent */
+    void SetTotalSpent(float newTotalSpent);
+
+    /* AddPurchase - adds newPurchase to totalSpent (i.e. a new transaction) */
+    void AddPurchase(double newPurchase);
+
+    bool operator<(Member compareto) const;
+    bool operator>(Member compareto) const;
+private:
+    QString name;
+    int    idNum;
+    QString memberType;
+    Date   expDate;
+    float  totalSpent;
+};
+
+
+#endif // MEMBER_H
