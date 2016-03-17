2496cec40103d3534d78e0b718f01b8edb10a3cf
KermiteO <kermicheo@gmail.com>
2016-03-17 15:14:21 -0700

    fixed up so compiles

--- Executive.cpp
+++ Executive.cpp
@@ -1,42 +1,61 @@
 #include "Executive.h"
 
-Executive::Executive() :Regular()
+Executive::Executive() :Member()
 {
     rebate = 0;
-    memberType = "Executive";
+    GetNewType() = "Executive";
 }
 
 Executive::Executive(QString newName,
                      double  newNum,
                      Date    newDate,
                      float   newSpent)
-                     :Regular(newName,newNum,newDate,newSpent)
+                     :Member(newName,newNum,newDate,newSpent)
 {
     UpdateRebate();
-    memberType = "Executive";
+    GetNewType() = "Executive";
 }
 
 Executive::~Executive() {}
 
 void Executive::SetMember(QString newName,
                           double  newNum,
-                          QString newType,
                           Date    newDate,
                           float   newSpent)
 {
-    Regular::SetMember(newName,newNum,newDate,newSpent);
+    Member::SetMember(newName,newNum,newDate,newSpent);
     UpdateRebate();
 }
 
 void Executive::SetTotalSpent(float newSpent)
 {
-    this->totalSpent = newSpent;
+    Member::SetTotalSpent(newSpent);
     UpdateRebate();
 }
 
 void Executive::UpdateRebate()
 {
-    rebate = this->totalSpent * REBATE_AMT;
+    rebate = this->GetTotalSpent() * REBATE_AMT;
+}
+
+QString Executive::GetName() const
+{
+    return Member::GetName();
+}
+
+double Executive::GetId() const
+{
+    return Member::GetId();
+}
+
+QString Executive::GetNewType() const
+{
+    return Member::GetMemberType();
+}
+
+float Executive::GetTotalSpent() const
+{
+    return Member::GetTotalSpent();
 }
 
 float Executive::GetRebate() const
